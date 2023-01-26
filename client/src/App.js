ecdb1ab8dc6a06034136ba506e47b9faf586f8b7
Agrima10 <agrimabundela10@gmail.com>
2023-01-26 15:30:10 +0530

    dw

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar/navbar.jsx';
+import Members from './components/team/team_members.jsx';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
+
+      <Members></Members>
     </div>
   );
 }
-
 export default App;
---
+++ client/src/components/Fill_Details/Fill.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Fill() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Fill
