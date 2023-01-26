fbbbe81aa0b053093bfe06522f9856bb26826276
A5HU705H <104879825+A5HU705H@users.noreply.github.com>
2023-01-26 14:58:49 +0530

    Add files via upload

---
+++ client/src/components/team/members.js
@@ -0,0 +1,67 @@
+const Members= [
+    {
+        Name: 'Ashutosh',
+        Desc: 'this is something about me, basically somewhat of a flavor-text',
+        Linkedin: '#',
+        Facebook: '#',
+        Instagram: '#',
+        Google: '#',
+        img: '/images/grey.png'
+    },
+    {
+        Name: 'Agrima',
+        Desc: 'this is something about me, basically somewhat of a flavor-text',
+        Linkedin: '#',
+        Facebook: '#',
+        Instagram: '#',
+        Google: '#',
+        img: '/images/grey.png'
+    },
+    {
+        Name: 'Aditi',
+        Desc: 'this is something about me, basically somewhat of a flavor-text',
+        Linkedin: '#',
+        Facebook: '#',
+        Instagram: '#',
+        Google: '#',
+        img: '/images/grey.png'
+    },
+    {
+        Name: 'Anurag',
+        Desc: 'this is something about me, basically somewhat of a flavor-text',
+        Linkedin: '#',
+        Facebook: '#',
+        Instagram: '#',
+        Google: '#',
+        img: '/images/grey.png'
+    },
+    {
+        Name: 'Harsh',
+        Desc: 'this is something about me, basically somewhat of a flavor-text',
+        Linkedin: '#',
+        Facebook: '#',
+        Instagram: '#',
+        Google: '#',
+        img: '/images/grey.png'
+    },
+    {
+        Name: 'Safdar',
+        Desc: 'this is something about me, basically somewhat of a flavor-text',
+        Linkedin: '#',
+        Facebook: '#',
+        Instagram: '#',
+        Google: '#',
+        img: '/images/grey.png'
+    },
+    {
+        Name: 'Sonika',
+        Desc: 'this is something about me, basically somewhat of a flavor-text',
+        Linkedin: '#',
+        Facebook: '#',
+        Instagram: '#',
+        Google: '#',
+        img: '/images/grey.png'
+    }
+]
+
+export default Members;
---
+++ client/src/components/team/team_members.jsx
@@ -0,0 +1,30 @@
+import Members from './members';
+import React, {Component} from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './team_members.css';
+
+
+class Cards extends Component {
+    render(){
+    return(
+        <div className="cards">
+            {Members.map((member) => {
+                
+                return(
+                <Card post={member} key={member.Name} style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={member.img} />
+                <Card.Body>
+                  <Card.Title >{member.Name}</Card.Title>
+                  <Card.Text>{member.Desc}</Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+                )          
+            })}
+        </div>
+    );
+        }
+};
+
+export default Cards;
