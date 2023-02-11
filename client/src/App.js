27505a5a0b0ca901145a08f178dcdb5831b055d5
Ghost4681 <64606908+Ghost4681@users.noreply.github.com>
2023-02-11 18:57:38 +0530

    made homepage responsive

--- client/src/App.js
+++ client/src/App.js
@@ -113,10 +113,10 @@ console.log(authData);
 
   return (
     <LoginContext.Provider value={{loggedin, setLoggedin, user, setUser, authData, setAuthData}}>
-    <div className="App">
-      <Navbar/>
+    <div className="App overflow-x-hidden">
+      {/* <Navbar/> */}
       <Routes>
-      <Route exact path="/" element={<Homepage/>} />
+      <Route exact path="/" element={<div className='overflow-x-hidden'><Homepage/></div>} />
       <Route exact path="/fill" element={<Fill />} />
       <Route exact path="/profile" element={<SecondLogin />} />
       <Route exact path="/about" element={<About />} />
