f11c365ce5729fd05921f11131e48643a6bebd1b
ayushawasthi24 <116417909+ayushawasthi24@users.noreply.github.com>
2023-03-16 00:17:17 +0530

    changes

--- client/src/components/team/Cards.jsx
+++ client/src/components/team/Cards.jsx
@@ -1,50 +1,51 @@
-import Members from './members';
-import React, {Component, useContext, useEffect} from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import loadingSpinner from '../Homepage/images/808.gif'
-import './Cards.scss';
-import { LoginContext } from '../../helpers/Context';
-function Cards(){
-    const {loading, setLoading} = useContext(LoginContext);
-    
-    useEffect(() => {
-      setLoading(true);
-      const Load = async () => {
-          await new Promise((r) => setTimeout(r, 1000));
-  
-          setLoading((loading) => !loading);
-      }
-  
-      Load();
-  }, [])
+import Members from "./members";
+import React, { Component, useContext, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import loadingSpinner from "../Homepage/images/808.gif";
+import "./Cards.scss";
+import { LoginContext } from "../../helpers/Context";
+function Cards() {
+  const { loading, setLoading } = useContext(LoginContext);
 
-    return(
-      <>
-      {loading &&
-            <div className='spinner'>
-            <span class="loader"></span>
-            </div>
-            }
-      {!loading &&
+  useEffect(() => {
+    setLoading(true);
+    const Load = async () => {
+      await new Promise((r) => setTimeout(r, 1000));
+
+      setLoading((loading) => !loading);
+    };
+
+    Load();
+  }, []);
+
+  return (
+    <>
+      {loading && (
+        <div className="spinner">
+          <span class="loader"></span>
+        </div>
+      )}
+      {!loading && (
         <div className="cards">
-            {Members.map((member) => {
-                
-                return(
-                <Card post={member} key={member.Name} style={{ width: '18rem' }}>
+          {Members.map((member) => {
+            return (
+              <Card post={member} key={member.Name} style={{ width: "18rem" }}>
                 <Card.Img variant="top" src={member.img} />
                 <Card.Body>
-                  <Card.Title >{member.Name}</Card.Title>
+                  <Card.Title>{member.Name}</Card.Title>
                   <Card.Text>{member.Desc}</Card.Text>
-                  <Button variant="primary" id='go' style={{ width :"70%"}}>Go somewhere</Button>
+                  <Button variant="primary" id="go" style={{ width: "70%" }}>
+                    Go somewhere
+                  </Button>
                 </Card.Body>
               </Card>
-                )          
-            })}
-        </div>}
-      </>
-    );
-        
-};
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+}
 
 export default Cards;
