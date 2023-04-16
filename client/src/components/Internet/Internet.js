eb153cc0e4603bc8193fd060608b66821d058cd8
Agrima10 <agrimabundela10@gmail.com>
2023-04-17 01:43:07 +0530

    .

---
+++ client/src/components/Internet/Internet.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Internet.scss'
+function Error() {
+  return (
+    <>
+      <div className="containererr">
+        <h1 id="err">ERROR 404 </h1>
+        <h1 id="page">- Internet Issue </h1>
+        <p>Please check your internet connection.</p>
+      </div>
+    </>
+  )
+}
+
+export default Error
